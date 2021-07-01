7f0542b91477bf6a31ad82eb4b461bd1ccfc8542
Raúl Molina Hernández <raul@z1.digital>
2021-07-01 17:17:36 +0200

    feat: update test

---
+++ cypress/integration/1-getting-started/sample_spec.js
@@ -0,0 +1,8 @@
+describe('My First Test', () => {
+
+  it('clicks the link "type"', () => {
+    cy.visit("https://example.cypress.io");
+    cy.contains('type').click();
+    cy.url().should('include', '/commands/actions');
+  })
+})
