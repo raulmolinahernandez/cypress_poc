66c6151fae12344e6d378122312c49c0e3ca38bf
Raúl Molina Hernández <raul@z1.digital>
2021-08-02 10:07:11 +0200

    feat: update test

--- cypress/integration/1-getting-started/sample_spec.js
+++ cypress/integration/1-getting-started/sample_spec.js
@@ -1,8 +1,10 @@
 describe('My First Test', () => {
 
-  it('clicks the link "type"', () => {
+  it('Gets, types and asserts', () => {
     cy.visit("https://example.cypress.io");
     cy.contains('type').click();
     cy.url().should('include', '/commands/actions');
+    cy.get('.action-email').type('fake@email.com').should('have.value', 'fake@email.com');
+
   })
 })
